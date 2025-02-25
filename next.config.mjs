2cc67f68d62f23ad0e5743def3a1df668136681c
Nidhi-Phophaliya <nidhiphophaliya945@gmail.com>
2025-02-25 17:36:48 +0530

    ignore ts error

--- next.config.mjs
+++ next.config.mjs
@@ -1,6 +1,9 @@
 /** @type {import('next').NextConfig} */
 
 const nextConfig = {
+  typescript:{
+    ignoreBuildErrors: false,
+  },
   experimental: {
     serverActions: true,
     serverComponentsExternalPackages: ["mongoose"],
